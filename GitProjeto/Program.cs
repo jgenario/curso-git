f3a52a2246029288d5ba526564c201bbd1240978
Jose Genario <jgenario@gmail.com>
2021-02-03 20:44:09 -0300

    Projeto iniciado , mostrando a mensagem ola mundo

---
+++ GitProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
